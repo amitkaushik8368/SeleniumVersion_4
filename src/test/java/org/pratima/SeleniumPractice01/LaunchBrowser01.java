f3719e22331b7946e2e8ff294deb6aee2629c237
amitkaushik8368 <amit.kaushik9466@gmail.com>
2025-03-23 16:30:30 +0530

    March-23 Afternoon commit

--- src/test/java/org/pratima/SeleniumPractice01/LaunchBrowser01.java
+++ src/test/java/org/pratima/SeleniumPractice01/LaunchBrowser01.java
@@ -1,3 +1,17 @@
 package org.pratima.SeleniumPractice01;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LaunchBrowser01
+{
+    @Test
+    public void runTheTest() {
+
+        ChromeDriver chromeDriver = new ChromeDriver();
+        chromeDriver.get("https://www.linkedin.com/in/amit-sharma-qa/");
+        System.out.println(chromeDriver.getTitle());
+        chromeDriver.quit();
+    }
+
 }
