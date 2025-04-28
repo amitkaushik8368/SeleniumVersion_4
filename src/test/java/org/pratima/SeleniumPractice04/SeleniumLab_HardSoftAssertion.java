f7b430f73d4b2f05cc39f8b4ab9e408c1d28c4b6
amitkaushik8368 <amit.kaushik9466@gmail.com>
2025-04-28 21:59:46 +0530

    April 28 commit2

---
+++ src/test/java/org/pratima/SeleniumPractice04/SeleniumLab_HardSoftAssertion.java
@@ -0,0 +1,21 @@
+package org.pratima.SeleniumPractice04;
+
+import org.pratima.BoilerPlateClass;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+import org.testng.asserts.SoftAssert;
+
+public class SeleniumLab_HardSoftAssertion extends BoilerPlateClass
+{
+    @Test
+    public void hardSoftAssert()
+    {
+
+        SoftAssert softAssert = new SoftAssert();
+        Assertion assertion = new Assertion();
+
+
+    }
+
+}
